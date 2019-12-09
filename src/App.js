9422097b7da382a58cf6179d287bcc2bc2831451
AidiRi <rivera.aidi@gmail.com>
2019-12-08 19:11:12 -0800

    React Router paths added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <Router>
+      <div>
+
+        <Route exact path='/' render={() => <div>Home</div>} />
+        <Route path='/projects' render={() => <div>Projects</div>} />
+        <Route path='/blog' render={() => <div>Blog</div>} />
+        <Route path='/resume' render={() => <div>Resume</div>} />
+      </div>
+    </Router>
   );
 }
 
--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Blogs = () => {
+const Blog = () => {
   return(
 
   )
