8b745bd4819d87f7182e15a65b511cef6cf77f6e
AidiRi <rivera.aidi@gmail.com>
2019-12-22 16:34:01 -0800

    Changed thumbnail styling to inline styles

--- src/Components/projects/thumbnail.js
+++ src/Components/projects/thumbnail.js
@@ -1,24 +1,55 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
-import './style.css'
+import style from './style.css'
+
 //Props: Title, Img, Desc
+
 const Thumbnail = (props) => {
   return(
 
-    <section className="project-container">
+    <section style={styles.Container}>
       <Link to="">
-        <header className="project-title">{props.Title}</header>
+        <header style={styles, styles.Header}>{props.Title}</header>
       </Link>
 
       <Link to="">
-        <img className="project-image" src={props.Img} alt="Project Image" />
+        <img style={styles.Img} src={props.Img} alt="Project Image" />
       </Link>
 
-      <p className="project-description">{props.Desc}</p>
+      <p style={styles}>{props.Desc}</p>
 
     </section>
 
   )
+}
+
+const styles={
+  color: "black",
+  Header: {
+    textAlign: "left",
+    color: "black",
+    textDecoration: "underline",
+    fontWeight: "bold",
+    fontSize: "20px",
+  },
+  Img: {
+    display: "block",
+    margin: "0 auto",
+    maxWidth: "70%",
+  },
+  Container: {
+    display: "flex",
+    flexDirection: "column",
+    height: "400px",
+    width: "90%",
+    maxWidth: "400px",
+    minWidth: "300px",
+    backgroundColor: "whitesmoke",
+    textAlign: "center",
+    margin: "30px auto",
+    padding: "10px",
+
+  }
 }
 
 export default Thumbnail;
